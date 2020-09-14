e7982f7021d24c58fc196c689afcba517b30327a
abccsss <buchenjing@outlook.com>
2020-09-15 00:20:12 +0800

    Add margin for printing

--- build.py
+++ build.py
@@ -1,7 +1,10 @@
 import json
+import os
 import re
+import shutil
 import subprocess
 from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2.pdf import PageObject
 
 def main():
     with open('contents.json') as f:
@@ -90,9 +93,39 @@ def main():
             writer.appendPagesFromReader(reader)
             if (reader.numPages % 2 == 1):
                 writer.addBlankPage()
-            
+    
+    if (os.path.exists('.temp')):
+        shutil.rmtree('.temp')
+    os.mkdir('.temp')
+    with open('.temp/hesi.pdf', 'wb') as f:
+        writer.write(f)
+
+    # Merge page template
+    with open('.temp/temp.tex', 'w') as f:
+        f.write('\\documentclass{article}\\usepackage[paperwidth=210mm,paperheight=297mm,left=0mm,right=0mm,top=0mm,bottom=0mm]{geometry}\\usepackage{tikz}\\begin{document}\\noindent\\begin{tikzpicture}\\draw[white] (0mm,0mm) rectangle (210mm,-290mm);\\draw[thick](2.5mm,-30mm)--(12.5mm,-30mm)(2.5mm,-265mm)--(12.5mm,-265mm)(207.5mm,-30mm)--(197.5mm,-30mm)(207.5mm,-265mm)--(197.5mm,-265mm)(27.5mm,-5mm)--(27.5mm,-15mm)(27.5mm,-290mm)--(27.5mm,-280mm)(182.5mm,-5mm)--(182.5mm,-15mm)(182.5mm,-290mm)--(182.5mm,-280mm);\\end{tikzpicture}\\end{document}')
+    proc = subprocess.Popen([
+        'latexmk', '-interaction=nonstopmode', '-pdf', '-xelatex', 'temp.tex'
+    ], cwd='.temp', stdout=subprocess.DEVNULL)
+    proc.wait(timeout=60)
+
+    print('============')
+    print('Adding page margin...')
+    print('============')
+
+    writer = PdfFileWriter()
+    reader1 = PdfFileReader(open('.temp/hesi.pdf', 'rb'))
+    reader2 = PdfFileReader(open('.temp/temp.pdf', 'rb'))
+    templatePage = reader2.getPage(0)
+    mm = float(templatePage.mediaBox.getWidth() / 210)
+    for i in range(reader1.numPages):
+        page = PageObject.createBlankPage(width=210 * mm, height=297 * mm)
+        page.mergePage(reader2.getPage(0))
+        page.mergeTranslatedPage(reader1.getPage(i), 27.5 * mm, 30 * mm)
+        writer.addPage(page)
     with open('hesi.pdf', 'wb') as f:
         writer.write(f)
+
+    shutil.rmtree('.temp')
 
     print('============')
     print('Output written to {} ({} + {} pages)'.format(
